37fc09f183ff21a243fef0ccb41d81b1c4479bc4
Jojo@UMv3 <jonas.marcello@esbme.com>
2019-09-02 07:21:33 +0200

    Initial commit

---
+++ pyhpo/ontology.py
@@ -0,0 +1,130 @@
+from pyhpo.term import HPOTerm
+
+
+class Ontology():
+    def __init__(self, filename=None):
+        self._map = {}
+
+        if filename:
+            self.load_from_file(filename)
+
+    def load_from_file(self, filename):
+        term = None
+        with open(filename) as fh:
+            for line in fh:
+                line = line.strip()
+                if line == '[Term]':
+                    if term is not None:
+                        self.append(term)
+                    term = HPOTerm()
+                    continue
+                if not term:
+                    continue
+                term.add_line(line)
+            self.append(term)
+        self.connect_all()
+
+    def append(self, item):
+        self._map[item._index] = item
+
+    def connect_all(self):
+        for term in self._map.values():
+            for parent in term.parent_ids():
+                term.parents = self._map[parent]
+                self._map[parent].children = term
+
+    def search(self, query):
+        for term in self:
+            if query in term.name or self.synonym_search(term, query):
+                yield term
+
+    def synonym_search(self, term, query):
+        for synonym in term.synonym:
+            if query in synonym:
+                return True
+        return False
+
+    def match(self, query):
+        for term in self:
+            if query == term.name:
+                return term
+        raise RuntimeError('No HPO entry with name {}'.format(query))
+
+    def synonym_match(self, query):
+        """
+        Searches for actual and synonym term matches
+        If a match is found in any term, that one is returned
+        If no actual match is found, the first match
+        with synonyms is considered
+        """
+        synonym_hit = None
+        for term in self:
+            if query == term.name:
+                return term
+            if not synonym_hit and query in term.synonym:
+                synonym_hit = term
+
+        if synonym_hit:
+            return synonym_hit
+        raise RuntimeError('No HPO entry with term or synonym {}'.format(query))
+
+    def path(self, query1, query2):
+        term1 = self.get_hpo_object(query1)
+        term2 = self.get_hpo_object(query2)
+
+        # set of all parents for term-1
+        parents1 = set()
+        for path in term1.hierarchy():
+            parents1.update(path)
+
+        # set of all parents for term-2
+        parents2 = set()
+        for path in term2.hierarchy():
+            parents2.update(path)
+
+        # common ancestors
+        common = parents1 & parents2
+
+        paths = []
+        for term in common:
+            path1 = term1.shortest_path_to_parent(term)
+            path2 = term2.shortest_path_to_parent(term)
+            total_path = path1[1] + tuple(reversed(path2[1]))[1:]
+            paths.append((
+                path1[0] + path2[0],
+                total_path,
+                path1[0],
+                path2[0]
+            ))
+        return sorted(paths, key=lambda x: x[0])[0]
+
+    def get_hpo_object(self, query):
+        if isinstance(query, str):
+            return self.synonym_match(query)
+
+        if isinstance(query, int):
+            return self[query]
+
+        raise SyntaxError('Invalid type {} for parameter "query"'.format(
+            type(query)
+        ))
+
+    def __getitem__(self, key):
+        if key in self._map:
+            return self._map[key]
+        else:
+            return None
+
+    def __len__(self):
+        return len(self._map.keys())
+
+    def __iter__(self):
+        return iter(self._map.values())
+
+
+# o = Ontology()
+# for term in terms:
+#     o.append(term)
+# o.connect_all()
+# 
+# 
---
+++ pyhpo/term.py
@@ -0,0 +1,293 @@
+class HPOTerm():
+    """
+    Represents an HPO Term
+    with all metadata
+
+    Initilized as empty object
+    add info line by line from OBO file
+
+    Attributes
+    ----------
+    name: str
+        HPO Term name
+        Example: Abnormality of body heigh
+    id: str
+        HPO Term ID
+        Example: HP:0000003
+    _index: int
+        Integer representation of ID
+        Example: 3
+    definition: str
+        HPO Term Definition
+        Example: "Multicystic dysplasia of the kidney is characterized by multiple cysts of varying size in the kidney and the absence of a normal pelvicaliceal system. The condition is associated with ureteral or ureteropelvic atresia, and the affected kidney is nonfunctional." [HPO:curators]
+    comment: str
+        Comments from HPO Team
+        Example: Multicystic kidney dysplasia is the result of abnormal fetal renal development in which the affected kidney is replaced by multiple cysts and has little or no residual function. The vast majority of multicystic kidneys are unilateral. Multicystic kidney can be diagnosed on prenatal ultrasound.
+    synonym: list(str)
+        List of synonymous names
+        Example: ['Multicystic dysplastic kidney', 'Multicystic kidneys', 'Multicystic renal dysplasia']
+    xref: list
+        List of xref attributes
+    is_a: list(str)
+        List of parent HPO terms
+        Example: ['HP:0000107 ! Renal cyst']
+
+    """
+    def __init__(self):
+        self.name = None
+        self.definition = None
+        self.comment = None
+        self._id = None
+        self._index = 0
+
+        self._alt_id = []
+        self._alt_index = []
+        self._synonym = []
+        self._xref = []
+        self._is_a = []
+        self._parents = []
+        self._children = []
+
+    def add_line(self, line):
+        if line == '':
+            return
+        key, *values = line.split(': ')
+        value = ': '.join(values)
+        if key == 'def':
+            key = 'definition'
+        self.__setattr__(key, value)
+
+    def __index__(self):
+        return self._index
+
+    __int__ = __index__
+
+    __hash__ = __index__
+
+    def __eq__(self, other):
+        return isinstance(other, HPOTerm) and self.__hash__() == other.__hash__()
+
+    def __lt__(self, other):
+        return self.__int__() < int(other)
+
+    def __str__(self):
+        return '{} | {}'.format(self._id, self.name)
+
+    def __repr__(self):
+        return '\n'.join([
+            '\n[Term]',
+            'id: {}'.format(self._id),
+            'name: {}'.format(self.name),
+            'def: {}'.format(self.definition),
+            'comment: {}'.format(self.comment),
+            '\n'.join('synonym: {}'.format(item) for item in self._synonym),
+            '\n'.join('xref: {}'.format(item) for item in self._xref),
+            '\n'.join('is_a: {}'.format(item) for item in self._is_a),
+            '\n'
+        ])
+
+    @property
+    def id(self):
+        return self._id
+
+    @id.setter
+    def id(self, value):
+        if self._id and self._id != value:
+            raise RuntimeError('Unable to update existing ID')
+        self._id = value
+        self._index = HPOTerm.id_from_string(value)
+
+    @property
+    def alt_id(self):
+        return self._alt_id
+
+    @alt_id.setter
+    def alt_id(self, value):
+        self._alt_id.append(value)
+        self._alt_index.append(HPOTerm.id_from_string(value))
+
+    @property
+    def synonym(self):
+        return self._synonym
+
+    @synonym.setter
+    def synonym(self, value):
+        self._synonym.append(HPOTerm.parse_synonym(value))
+
+    @property
+    def xref(self):
+        return self._xref
+
+    @xref.setter
+    def xref(self, value):
+        self._xref.append(value)
+
+    @property
+    def is_a(self):
+        return self._is_a
+
+    @is_a.setter
+    def is_a(self, value):
+        self._is_a.append(value)
+
+    @property
+    def parents(self):
+        return self._parents
+
+    @parents.setter
+    def parents(self, hpo):
+        self._parents.append(hpo)
+
+    @property
+    def children(self):
+        return self._children
+
+    @children.setter
+    def children(self, hpo):
+        self._children.append(hpo)
+
+    def parent_ids(self):
+        """
+        List of IDs of parent HPO Terms
+
+        Returns:
+        --------
+        list(int)
+            Example: [107]
+        """
+        return [
+            HPOTerm.id_from_string(val) for val in self.is_a
+        ]
+
+    def count_parents(self):
+        """
+        Calculates total number of ancestral HPO Terms
+
+        Returns
+        -------
+        int
+            The number of all ancestral HPO Terms
+        """
+        return sum([
+            parent.count_parents()
+            for parent in self.parents
+        ])+1
+
+    def print_hierarchy(self, indent=0, indent_increase=2):
+        """
+        Prints hierarchy diagram of current and
+        all ancestral HPO Terms
+
+        Parameters
+        ----------
+        indent: int
+            Number of spaces for current indentation level
+
+        indent_increase: int
+            Number of spaces to increase the next indentation level
+
+        Returns
+        -------
+        None
+        """
+
+        print('{}-{}'.format(' '*indent, self.name))
+        for parent in self.parents:
+            parent.print_hierarchy(indent + indent_increase, indent_increase)
+
+    def hierarchy(self, root=None, paths=None):
+        """
+        Calculates all paths from current term to Root term
+        and returns each path as a Tuple of HPOTerms
+
+        Parameters
+        ----------
+        root: list
+            List of children HPO Terms
+
+        paths: list
+            List of all possible paths from base to the root HPOTerm
+
+        Returns
+        -------
+        tuple
+            Tuple of paths. Each path is another tuple made up of HPOTerms
+        """
+        if root is None:
+            root = []
+        if paths is None:
+            paths = []
+        local_root = root + [self]
+        for parent in self.parents:
+            parent.hierarchy(local_root, paths)
+        if not self.parents:
+            paths.append(tuple(local_root))
+        return tuple(paths)
+
+    def longest_path_to_root(self):
+        """
+        Calculates the longest path to root
+
+        Returns
+        -------
+        int
+            Maximum number of nodes until the root HPOTerm
+        """
+        return max([
+            len(h) for h in self.hierarchy()
+        ])
+
+    def shortest_path_to_root(self):
+        """
+        Calculates the shortest path to root
+
+        Returns
+        -------
+        int
+            Minimum number of nodes until the root HPOTerm
+        """
+        return min([
+            len(h) for h in self.hierarchy()
+        ])
+
+    def shortest_path_to_parent(self, other):
+        """
+        Calculates the shortest path to another HPO Term
+
+        Parameters
+        ----------
+        other: HPOTerm
+            Other HPO Term
+
+        Returns
+        -------
+        int
+            Minimum number of nodes until the specified HPOTerm
+        """
+        steps = float('inf')
+        shortest_path = None
+        for path in self.hierarchy():
+            for i, term in enumerate(path):
+                if term == other:
+                    if i < steps:
+                        steps = i
+                        shortest_path = path[:i+1]
+        return (steps, shortest_path)
+
+    def longest_path_to_bottom(self, level=0):
+        if len(self.children):
+            return max([
+                child.longest_path_to_bottom(level + 1) for child in self.children
+            ])
+        else:
+            return level
+
+    @staticmethod
+    def id_from_string(hpo_string):
+        # removes the term (if present)
+        idx = hpo_string.split('!')[0].strip()
+        return int(idx.split(':')[1].strip())
+
+    @staticmethod
+    def parse_synonym(synonym):
+        return synonym.split('"')[1]
