9f82a1cc3aa45c758a70df5ed8a20ee50f8164f9
Jojo@UMv3 <jonas.marcello@esbme.com>
2020-07-26 12:01:25 +0200

    Added benchmarking with BasicHPOSet

--- tests/benchmarking.py
+++ tests/benchmarking.py
@@ -2,13 +2,20 @@
 import cProfile
 
 from pyhpo.ontology import Ontology
-from pyhpo.set import HPOSet
+from pyhpo.set import HPOSet, BasicHPOSet
 
 
 def diseases2sets(diseases):
     return [(
         d.name,
         HPOSet.from_queries(d.hpo).child_nodes().remove_modifier()
+    ) for d in diseases]
+
+
+def diseases2basicsets(diseases):
+    return [(
+        d.name,
+        BasicHPOSet.from_queries(d.hpo)
     ) for d in diseases]
 
 
@@ -21,3 +28,6 @@ cProfile.run('build_ontology()')
 
 print('===== BUILDING DISEASE SETS ======')
 cProfile.run('diseases2sets(Ontology.omim_diseases)')
+
+print('===== BUILDING DISEASE BasicSETS ======')
+cProfile.run('diseases2basicsets(Ontology.omim_diseases)')
