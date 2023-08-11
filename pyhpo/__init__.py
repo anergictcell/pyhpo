5ef40645bc8e6b94e8e64ec7d07210a5182403f9
Jonas Marcello <jonas.marcello@centogene.com>
2023-08-11 21:13:03 +0200

    Hotfix to include missing submodules during build process

--- pyhpo/__init__.py
+++ pyhpo/__init__.py
@@ -11,7 +11,7 @@ from pyhpo import config
 
 # The following info will be used by setup.py and sphinx documentation
 __author__ = "Jonas Marcello"
-__version__ = "3.2.3"
+__version__ = "3.2.4"
 
 __all__ = (
     "Annotation",
